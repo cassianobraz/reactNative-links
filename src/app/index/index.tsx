ab0103ab47a78b02fc277ee1f3dab3fbe27682bb
cassianobraz <cassanopb@gmail.com>
2026-02-15 02:03:08 -0400

    feat: links and categories

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,10 +1,10 @@
 import { MaterialIcons } from "@expo/vector-icons";
 import { Image, TouchableOpacity, View } from "react-native";
 
+import { Categories } from "@/components/categories";
+import { Link } from '@/components/link';
 import { colors } from '@/styles/colors';
 import { styles } from "./styles";
-
-import { Category } from "@/components/category";
 
 export default function Index() {
   return (
@@ -13,11 +13,13 @@ export default function Index() {
         <Image source={require('@/assets/logo.png')} style={styles.logo} />
 
         <TouchableOpacity>
-          <MaterialIcons name="add" size={32} color={colors.green[ 300 ]} />
+          <MaterialIcons name="add" size={32} color={colors.green[300]} />
         </TouchableOpacity>
       </View>
-      
-      <Category />
+
+      <Categories />
+
+      <Link name="Rocketseat" url="https://www.rocketseat.com.br/" onDetails={() => console.log("Clicou aqui!")} />
     </View>
   )
 }
--- src/components/category/index.tsx
+++ src/components/category/index.tsx
@@ -1,13 +1,20 @@
 import { colors } from '@/styles/colors';
 import { MaterialIcons } from "@expo/vector-icons";
-import { Pressable, Text } from "react-native";
+import { Pressable, PressableProps, Text } from "react-native";
 import { styles } from './styles';
 
-export function Category() {
+type props = PressableProps & {
+  name: string;
+  isSelected: boolean;
+  icon: keyof typeof MaterialIcons.glyphMap;
+}
+
+export function Category({ name, icon, isSelected, ...rest }: props) {
+  const color = isSelected ? colors.green[300] : colors.gray[400];
   return (
-    <Pressable>
-      <MaterialIcons name='code' size={16} color={colors.gray[400]}/>
-      <Text style={styles.name}>Projetos</Text>
+    <Pressable style={styles.container} {...rest}>
+      <MaterialIcons name={icon} size={16} color={color}/>
+      <Text style={[styles.name, { color }]}>{name}</Text>
     </Pressable>
   )
 }
